b166125e0aa23da4957a6923dd7d212f89631eea
Gerson Nazareth <gerson.nazareth@gmail.com>
2015-12-10 17:53:07 -0700

    Responsive animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  $('.js--section-skills').waypoint(function (direction) {
+  $('.js--section-skills').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
-    }
-    else {
+    } else {
       $('nav').removeClass('sticky');
     }
   })
@@ -11,17 +10,35 @@ $(document).ready(function() {
   // Navigation Scroll
 
   $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
+    });
+  });
+
+  // Mobile Nav
+
+  $('.js--mobile-nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
     }
   });
-});
 });
